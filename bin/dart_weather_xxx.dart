f423fb0920eaa4146f3012d9fd8850d903ab6639
gs <gs@xyz.de>
2026-05-14 21:27:07 +0200

    added LocationProvider

--- bin/dart_weather_xxx.dart
+++ bin/dart_weather_xxx.dart
@@ -1,10 +1,13 @@
-//import 'package:dart_weather_xxx/forecast_provider.dart';
+import 'package:dart_weather_xxx/forecast_provider.dart';
+import 'package:dart_weather_xxx/location_provider.dart';
 // take care: next line would lead to error "Can't use a relative path to import a library in 'lib'."
 // see https://gemini.google.com/share/f3d605a58226
-import '../lib/forecast_provider.dart';
+//import '../lib/forecast_provider.dart';
 
 void main(List<String> arguments) async {
-  var forecastProvider = ForecastProvider();
+  var locationProvider = LocationProvider();
+  locationProvider.selectLocation("Sydney");
+  var forecastProvider = ForecastProvider(locationProvider);
 
   bool succeeded = await forecastProvider.fetchHourlyForecast();
   if (!succeeded) {
@@ -12,6 +15,7 @@ void main(List<String> arguments) async {
     return;
   }
 
+  print("this is the forecast for ${locationProvider.selectedLocation.name}:");
   for (var e in forecastProvider.hourlyForecast) {
     print("${e.localTime}: ${e.temp}°C, ${e.precipAmount}mm, ${e.cloudCover}%");
   }
--- lib/forecast_provider.dart
+++ lib/forecast_provider.dart
@@ -3,9 +3,13 @@ import 'package:http/http.dart' as http;
 import 'package:lat_lng_to_timezone/lat_lng_to_timezone.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'location_provider.dart';
 import 'weather_data.dart';
 
 class ForecastProvider {
+  ForecastProvider(this._locationProvider);
+
+  final LocationProvider _locationProvider;
   List<WeatherData> hourlyForecast = [];
   bool _initialized = false;
 
@@ -20,14 +24,8 @@ class ForecastProvider {
     }
 
     hourlyForecast.clear();
-
-    // latitude and longitude of Schloß Johannisburg in Aschaffenburg
-    var lat = 49.97606;
-    var lon = 9.14163;
-
-    // latitude and longitude for Sydney
-    // var lat = -33.86785;
-    // var lon = 151.20732;
+    var lat = _locationProvider.selectedLocation.latitude;
+    var lon = _locationProvider.selectedLocation.longitude;
 
     // 'https://api.open-meteo.com/v1/forecast?latitude=49.97606&longitude=9.14163&hourly=temperature_2m,precipitation,cloud_cover&forecast_days=1';
     final url =
